6be33a81706838849fd23bf723aae0fd3a2f9160
Pranali6297 <102611277+Pranali6297@users.noreply.github.com>
2022-09-17 11:04:40 +0530

    Add files via upload

---
+++ UserFlowPages/Thankyou.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Thankyou(){
+    return(
+    <section id="book-a-table" class="book-a-table">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Thank <span> You..!!!</span></h2>
+          
+        </div>
+
+      </div>
+    </section>
+    )
+}
