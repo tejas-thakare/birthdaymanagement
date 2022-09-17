280ebb329617f254582e5ab3ebf31e641340d1cf
Siddharth Jeswani <61928506+Siddharth16c@users.noreply.github.com>
2022-09-17 14:02:19 +0530

    Add files via upload

---
+++ src/Admin/Requests.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function Requests()
+{
+    return(
+<body>
+    
+    
+    <h1 class="text-center">Requests</h1>
+    <br/>
+    <table class="table">
+        <thead>
+        <tr class="tablecolor">
+        <th>Vendor Id</th>
+          <th>Vendor name</th>
+          
+          <th>Login Id</th>
+          <th>Service</th>
+          <th>Rating</th>
+          <th>Approval</th>
+        </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td>1</td>
+                <td>Sham Singh</td>
+                
+                <td>Shamsingh231</td>
+                <td>Cake making</td>
+                <td>*****</td>
+                <td>
+                    <button href="#"  class="sid">&#10004;</button>&ensp; <button href="#" class="sid">&#10006;</button>
+                </td>
+            </tr>
+      </tbody>  
+    </table> 
+        
+           
+        
+        
+  </body>)
+}
---
+++ src/Admin/Users.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Users()
+{
+    return(
+
+    
+   <div class="php-email-form">
+    <h1 class="text-center">Users</h1>
+    <br/>
+    <table class="table">
+        <thead>
+        <tr class="tablecolor">
+          <th>User Id</th>
+          <th>User name</th>
+          <th>Contact</th>
+          <th>Email</th>
+          <th>Login Id</th>
+          
+        </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Ram Singh</td>
+                <td>8888888888</td>
+                <td>ramsingh231@gmail.com</td>
+                <td>ramsingh231</td>
+               
+            </tr>
+      </tbody>  
+    </table> 
+        
+        
+            </div>
+ )
+}
