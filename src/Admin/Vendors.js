3f59647432e86a36c54012006a11a416c80a160c
vishalthite2 <106861797+vishalthite2@users.noreply.github.com>
2022-09-17 14:05:22 +0530

    Vendor component

---
+++ src/Admin/Vendors.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Vendors()
+{
+    return(
+<body>
+    
+    
+    <h1 class="text-center"> Vendors</h1>
+    <br/>
+    <table class="table">
+        <thead>
+        <tr class="tablecolor">
+          <th>Vendor Id</th>
+          <th>Vendor name</th>
+          <th>Contact</th>
+          <th>Email</th>
+          <th>Login Id</th>
+          <th>Service</th>
+          
+        </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Sham Singh</td>
+                <td>8888888888</td>
+                <td>Shamsingh231@gmail.com</td>
+                <td>Shamsingh231</td>
+                <td>Cake making</td>
+                
+            </tr>
+      </tbody>  
+    </table> 
+   
+  </body>)
+}
