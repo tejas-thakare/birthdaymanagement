f69b336b744d1d6511d96cbf07994517005d6337
vishalthite2 <106861797+vishalthite2@users.noreply.github.com>
2022-09-20 12:10:08 +0530

    Final page of the app

---
+++ src/Components/Final.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+const Final = () => {
+  return (
+    <div class="finalbody">
+        <div class="birthdaycard">
+            <div class="cardfront"><h3 class="happy">THANK YOU&#128591;</h3>
+                <div class="balloons">
+                    <div class="balloonone"></div>
+                    <div class="balloontwo"></div>
+                    <div class="balloonthree"></div>
+                    <div class="balloonfour"></div>
+                </div>
+            </div>
+            <div class="cardinside">
+                <h3 class="back">Best wishes</h3>
+                <p class="finalp">Thanks for choosing us!</p>
+                <p class="regards">visit us again</p>
+            </div>
+        </div>
+    </div>
+
+
+  )
+}
+
+export default Final
